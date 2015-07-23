3386cbf9487d36f91541d2f14f91f32de00b96b7
TJ Chapley <tjchap2840@gmail.com>
2015-07-23 17:18:21 -0400

    Comments, WCLogs, Post

---
+++ _posts/2015-07-23-all-the-things.md
@@ -0,0 +1,20 @@
+---
+layout: post
+title: All the things
+---
+
+<h4>Progression Update</h4>
+As of Wednesday night, we are 12.5/13 normal and 5/13 heroic. Really good job overall guys. Attendance has been great and the attitude of the raid has been good.
+
+Congratulations to Enide, who has officially been added to the <a href="/raid_team/">raid team</a>. He has consistently shown up to raids and is doing a great job as our main Resto Shaman. We have two other new (or newish) members that are doing a great job fitting in and meshing well with the DK style of raiding. Smites and Cenderi have already show a great attitude towards the overall raid and loot rules. Keep up the good work all.
+
+I know there isn't 100% satisifaction with the way tier pieces are going out, but I'm doing it with the raid in mind. Let me know if you have any concerns and I will see what I can do, but this really is the best and quickest way.
+
+Keep up the good work all!
+
+<h4>Website Update</h4>
+I've done a ton of work to the site today. Some things you guys won't ever see so I won't go into those, but there are plenty of things you will.
+
+First of all, I have added a feed for the latest Warcraft Log posts!!! I'm extremely happy about this one. You'll see it just below the MMOC feed on the left. Right now it shows the last 5 logs posted to the guild calendar. Clicking on the item will take you to the log. It will get cleaned up a bit and made to look better as time goes on (calling Tresane). 
+
+I have also added discussions via Disqus. I recommend you make an account, but I have set it up to allow guests to post as well. Each post will have a comment section and I think that you can go to a "forum" of sorts that will have all the activity on one page. I don't know for sure as I am still figuring out the features of Disqus.
--- public/parser.js
+++ public/parser.js
@@ -2,79 +2,45 @@
  * parses any RSS/XML feed through Google and returns JSON data
  * source: http://stackoverflow.com/a/6271906/477958
  */
-function parseRSS(url, container) {
+function sideBar(container) {
+  var base_url = 'https://www.warcraftlogs.com:443/v1';
+  var api_key = '4e6b85c57f6b99e30c1e296575957e12';
+  var reports_url = base_url + '/reports/guild/Dragon%20Knight/Boulderfist/US?api_key=' + api_key;
   $.ajax({
-    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+    url: reports_url,
+    dataType: 'json',
+    success: function(raids) {
+      $(container).append("<span class='list-group-item'><b>Recent Raid Logs</b></span>");
+      var report_base = 'https://www.warcraftlogs.com/reports/';
+      for (var i = raids.length - 1; i >= raids.length - 5; i--) {
+        var raid = raids[i];
+        var title = raid.title;
+        var report_url = report_base + raid.id;
+        // var fight_url = base_url + '/report/fights/' + raid.id + '?api_key=' + api_key;
+        var thehtml = "<a href='" + report_url + "' target=_blank' class='list-group-item'>" + title + "</a>";
+        $(container).append(thehtml);
+      };
+    },
+    error: function() {
+      alert("error");
+    }
+  });
+
+  url = document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=';
+  url +=  encodeURIComponent('http://www.mmo-champion.com/external.php?do=rss&type=newcontent&sectionid=1&days=120&count=5');
+  $.ajax({
+    url: url,
     dataType: 'jsonp',
     success: function(data) {
-      // <span class="list-group-item"><b>MMO Champion News</b></span>
-
-      //console.log(data.responseData.feed);
-      $(container).html("<span class='list-group-item'><b>MMO Champion Latest News</b></span>");
+      $(container).append("<span class='list-group-item'><b>MMO Champion Latest News</b></span>");
 
 
       $.each(data.responseData.feed.entries, function(key, value) {
-        // var thehtml = '<h3><a href="'+value.link+'" target="_blank">'+value.title+'</a></h3>';
-        // $(container).append(thehtml);
         var thehtml = "<a href='" + value.link + "' target='_blank' class='list-group-item'>" + value.title + "</a>";
         $(container).append(thehtml);
       });
-      // alert(data.responseData.feed.title);
     }
   });
-}
 
-function parseAPI(url, container) {
-  // url = document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url);
-  $.ajax({
-
-    // The 'type' property sets the HTTP method.
-    // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-    type: 'GET',
-
-    // The URL to make the request to.
-    url: url,
-    dataType: "jsonp",
-    crossDomain: true,
-    // The 'contentType' property sets the 'Content-Type' header.
-    // The JQuery default for this property is
-    // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-    // a preflight. If you set this value to anything other than
-    // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-    // you will trigger a preflight request.
-    // contentType: 'application/json',
-
-    xhrFields: {
-      // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-      // This can be used to set the 'withCredentials' property.
-      // Set the value to 'true' if you'd like to pass cookies to the server.
-      // If this is enabled, your server must respond with the header
-      // 'Access-Control-Allow-Credentials: true'.
-      withCredentials: true
-    },
-
-    headers: {
-      // Set any custom headers here.
-      // If you set any non-simple headers, your server must include these
-      // headers in the 'Access-Control-Allow-Headers' response header.
-    },
-
-    success: function(data) {
-      // Here's where you handle a successful response.
-      for (var obj in data) {
-        console.log(obj + "=" + data[obj]);
-      }
-    },
-
-    error: function(data) {
-      // for (var obj in data) {
-      //   console.log(obj + "=" + data[obj]);
-      // }
-      alert("error");
-      // Here's where you handle an error response.
-      // Note that if the error was due to a CORS issue,
-      // this function will still fire, but there won't be any additional
-      // information about the error.
-    }
-  });
+  
 }
